7faf12a3cc9f7a528bfb8571f746547e3071a950
Yasirkhan26 <yk388195@gmail.com>
2025-05-10 15:20:43 +0500

    Created app.py test file

---
+++ src/test_app.py
@@ -0,0 +1,36 @@
+from fastapi.testclient import TestClient
+from app import app
+
+client = TestClient(app)
+
+def test_health_endpoint():
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "OK", "message": "SysPulse is live!"}
+
+def test_memory_metrics_endpoint():
+    response = client.get("/metrics/memory")
+    assert response.status_code == 200
+    data = response.json()
+    assert "total_gb" in data
+    assert "used_gb" in data
+    assert "free_gb" in data
+    assert "percent" in data
+    assert isinstance(data["percent"], (int, float))
+
+def test_disk_metrics_endpoint():
+    response = client.get("/metrics/disk")
+    assert response.status_code == 200
+    data = response.json()
+    assert "total_gb" in data
+    assert "used_gb" in data
+    assert "free_gb" in data
+    assert "percent" in data
+    assert isinstance(data["percent"], (int, float))
+
+def test_cpu_metrics_endpoint():
+    response = client.get("/metrics/cpu")
+    assert response.status_code == 200
+    data = response.json()
+    assert "cpu_percent" in data
+    assert isinstance(data["cpu_percent"], (int, float)) 
